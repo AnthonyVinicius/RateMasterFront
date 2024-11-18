5326695db630f602ba727d4ac81b844d2777df0d
Anthony Vinicius <91100881+AnthonyVinicius@users.noreply.github.com>
2024-11-18 20:19:10 -0300

    Firebase
    Firebase Install, firbase.js e DAOService

---
+++ RateMaster/src/firebase.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+let config = {
+    apiKey: "AIzaSyCvF3k4n1-Rfckm6W-t8z6y3HKAwfeOWGQ",
+    authDomain: "ratemasterif.firebaseapp.com",
+    projectId: "ratemasterif",
+    storageBucket: "ratemasterif.firebasestorage.app",
+    messagingSenderId: "221591868606",
+    appId: "1:221591868606:web:e2c1e14662b20c1edffaef",
+    measurementId: "G-G8SJ2ZY2G7"
+};
+
+const firebaseApp = initializeApp(config);
+
+export const firestore = getFirestore(firebaseApp);

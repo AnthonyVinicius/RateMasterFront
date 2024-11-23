705dd738e787afdcbaa989889f46432bac03bbca
Anthony Vinicius <91100881+AnthonyVinicius@users.noreply.github.com>
2024-11-23 17:41:18 -0300

    Finalização da HomePage

---
+++ RateMaster/src/components/UserReview.vue
@@ -0,0 +1,37 @@
+<script>
+export default {
+    name: 'UserReview',
+    props: {
+        comment: {
+            type: String,
+            required: true
+        }
+    }
+}
+</script>
+
+<template>
+    <div class="container-fluid d-flex flex-column align-items-center box rounded-4 mb-5">
+        <p class="lead text-white pt-4 pb-3 text-resize"> {{ comment }}</p>
+        <img class="img-profile rounded-circle" src="../assets/ProfileIcon.png" alt="Profile Icon">
+        <p class="lead text-white pb-3">User</p>
+    </div>
+</template>
+
+<style scoped>
+.box {
+    background-color: #2C3E50;
+    min-height: 200px; /* Altura mínima */
+    max-height: 250px; /* Altura máxima */
+    overflow: hidden; /* Esconde conteúdo que ultrapassar */
+    text-align: center;
+}
+
+
+.text-resize {
+    font-size: calc(14px + 0.2vw); /* Ajusta dinamicamente o tamanho com base na largura da viewport */
+    line-height: 1;
+    overflow: hidden;
+    text-overflow: ellipsis; /* Mostra "..." no final caso o texto seja muito grande */
+}
+</style>
--- RateMaster/src/views/HomePageView.vue
+++ RateMaster/src/views/HomePageView.vue
@@ -3,6 +3,9 @@
 import TheNavbar from '@/components/TheNavbar.vue';
 import TheFooter from '@/components/TheFooter.vue';
 import ReviewHomePage from '@/components/ReviewHomePage.vue';
+import UserReview from '@/components/UserReview.vue';
+import NavButton from '@/components/NavButton.vue';
+
 
 </script>
 
@@ -12,7 +15,7 @@ import ReviewHomePage from '@/components/ReviewHomePage.vue';
 
     <TheNavbar />
     <div class="container-fluid p-0">
-        <img src="../assets/Home.png" alt="Picture">
+        <img class="header-img" src="../assets/Home.png" alt="Picture">
     </div>
 
     <div class="container-fluid  p-5">
@@ -42,28 +45,46 @@ import ReviewHomePage from '@/components/ReviewHomePage.vue';
 
 
     <div class="container-fluid p-5 bg-box">
-        <div class="text-box text-center">
-            <h1 class="display-4 text-white">Evaluate with confidence.</h1>
-            <p class="lead text-white">Discover detailed and unbiased reviews to help you make informed purchasing
+        <div class="text-box text-center p-5">
+            <h1 class="display-4 text-white text-start pt-5">Evaluate with confidence.</h1>
+            <p class="lead text-white text-start pb-5">Discover detailed and unbiased reviews to help you make informed purchasing
                 decisions.</p>
         </div>
     </div>
 
 
 
-    <div class="container text-center p-3">
-        <div class="row g-4">
+    <div class="container text-center p-3 mb-5">
+        <div class="row g-4 pt-5 ">
             <div class="col">
-                <ReviewHomePage text="Custom solutions designed to meet your specific needs."
-                    comment="We offer a range of products that simplify performance evaluation." />
+                <UserReview
+                    comment="I absolutely loved this product! The quality is outstanding, and it exceeded my expectations. I will definitely recommend it to my friends and family." />
             </div>
             <div class="col">
-                <ReviewHomePage text="Our services ensure quality and efficiency in every project."
-                    comment="We are committed to delivering results that exceed your expectations." />
+                <UserReview
+                    comment="The product is decent for the price, but it didn’t quite meet my expectations. It works, but there’s room for improvement." />
             </div>
             <div class="col">
-                <ReviewHomePage text="The technology that transforms the way you evaluate products."
-                    comment="Innovation and convenience are the pillars of our solutions." />
+                <UserReview
+                    comment="The customer service was fantastic! They were quick to respond, friendly, and resolved my issue in no time. A+ experience!" />
+            </div>
+            <div class="container border border-4 mt-5 p-0 text-start">
+                <div class="row g-0">
+                    <div class="col-md-6 col-lg-6 d-flex">
+                        <div class="card-body p-5 p-lg-5">
+                            <div>
+                                <div class="mb-2 pb-1">
+                                    <h1 class="h1 fw-bold">Discover our amazing products.</h1>
+                                </div>
+                                <h5 class="fw-normal pb-3">Explore our range of productsand find the perfect solution for your needs.</h5>
+                                <NavButton text="Learn More" class="button" />
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-6 col-lg-6 d-none d-md-block">
+                        <img src="../assets/placeholder.png" alt="product-image" class="img-fluid product-image" />
+                    </div>
+                </div>
             </div>
         </div>
     </div>
@@ -72,11 +93,10 @@ import ReviewHomePage from '@/components/ReviewHomePage.vue';
 
     <div class="container-fluid p-5 bg-box">
         <div class="text-box">
-            <h1 class="display-4 text-white">Contact Us.</h1>
+            <h1 class="display-4 text-white text-start">Contact Us.</h1>
             <p class="lead text-white">Discover how our platform can help you.</p>
         </div>
     </div>
-
 
     <TheFooter />
 
@@ -87,18 +107,7 @@ import ReviewHomePage from '@/components/ReviewHomePage.vue';
     background-color: #2C3E50;
 }
 
-.icon-box {
-    display: inline-block;
-    color: #E74C3C;
-    padding-top: 20px;
-    font-size: 50px;
-}
-
-.text-color {
-    color: #2C3E50;
-}
-
-img {
+.header-img {
     width: 100%;
 }
 </style>

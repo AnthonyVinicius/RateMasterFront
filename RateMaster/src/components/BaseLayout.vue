579b53f9945102894b2a70916c2887d9ecc8f542
Vinícius Arruda <arrudavini25@gmail.com>
2025-02-02 18:36:40 -0300

    alterações nas validações dos campos

--- RateMaster/src/components/BaseLayout.vue
+++ RateMaster/src/components/BaseLayout.vue
@@ -3,6 +3,30 @@ import TheNavbar from './TheNavbar.vue';
 import TheFooter from './TheFooter.vue';
 import { provide } from 'vue';
 import { userData, initAuth } from '@/services/AuthService.js';
+import { configure, defineRule } from 'vee-validate';
+import { required, max, between, min } from '@vee-validate/rules';
+
+//definir regras do vee
+defineRule('required', required);
+defineRule('max', max);
+defineRule('min', min);
+defineRule('between', between);
+defineRule('email', value =>{
+  const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
+  return emailRegex.test(value) || 'O campo deve ser um email válido.';
+})
+
+configure({
+  generateMessage: (ctx) => {
+    const messages = {
+      required: `Este campo é obrigatório.`,
+      max: `Este campo deve ter no máximo ${ctx.rule.params[0]} caracteres`,
+      min: `Este campo deve ter no mínimo ${ctx.rule.params[0]} caracteres`,
+      email: `O campo ${ctx.field} deve ser um email válido`
+    };
+    return messages[ctx.rule.name || `O campo ${ctx.field} é inválido`]
+  }
+});
 
 provide('userData', userData);
 initAuth();
